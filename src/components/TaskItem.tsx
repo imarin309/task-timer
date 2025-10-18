4477d10a0e4acf0ea5eca9f921c4866fddfcbe71
imarin309 <16imarin309@gmail.com>
2025-10-18 22:29:28 +0900

    modif input

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import TaskName from "./TaskName";
 
 type Props = {
   status: string;
   name: string;
   color: string;
+  onUpdateTaskName?: (newName: string) => void;
 };
 
-// TODO: タイマー機能の実装
-const TaskItem: React.FC<Props> = ({ status, name, color }) => {
+const TaskItem: React.FC<Props> = ({ status, name, color, onUpdateTaskName }) => {
+  const [taskName, setTaskName] = useState(name);
+
+  const handleUpdateName = (newName: string) => {
+    setTaskName(newName);
+    if (onUpdateTaskName) {
+      onUpdateTaskName(newName);
+    }
+  };
+
   return (
     <div className={`task-item ${color}`}>
       <div className="task-status">{status}</div>
-      <TaskName TaskName={name} />
-      {/* <button className="task-button">start</button>
-      <button className="task-button">stop</button>
-      <div className="task-time">残：00:00</div> */}
+      <TaskName TaskName={taskName} onUpdate={handleUpdateName} />
     </div>
   );
 };
