ac0856a844e60301e1f82bd6fc999dfedc0556c5
imarin309 <16imarin309@gmail.com>
2025-10-18 18:59:05 +0900

    delete timer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import avatarImage from "../assets/anpan.png"; // 画像のインポート
 
+const greeting: string = 'お疲れ様だぽよ、いつも頑張ってるね'
+
 const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header-image">
         <img src={avatarImage} alt="キャラクター" className="header-img" />
       </div>
-      <div className="header-timer">あと 05:49 だよ</div>
+      <div className="header-timer">{greeting}</div>
     </header>
   );
 };
--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -6,14 +6,15 @@ type Props = {
   color: string;
 };
 
+// TODO: タイマー機能の実装
 const TaskItem: React.FC<Props> = ({ status, name, color }) => {
   return (
     <div className={`task-item ${color}`}>
       <div className="task-status">{status}</div>
       <div className="task-name">{name}</div>
-      <button className="task-button">start</button>
+      {/* <button className="task-button">start</button>
       <button className="task-button">stop</button>
-      <div className="task-time">残：00:00</div>
+      <div className="task-time">残：00:00</div> */}
     </div>
   );
 };
